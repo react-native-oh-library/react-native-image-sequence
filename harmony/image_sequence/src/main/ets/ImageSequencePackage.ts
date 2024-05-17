b38a06c6835e7dfea56c011ed97ce82689c50623
wuya_smile <liuderong3@h-partners.com>
2024-05-17 11:50:40 +0800

    修改采样率异常

--- harmony/image_sequence/src/main/ets/ImageSequencePackage.ts
+++ harmony/image_sequence/src/main/ets/ImageSequencePackage.ts
@@ -1,6 +1,6 @@
 import { RNPackage } from '@rnoh/react-native-openharmony/ts';
 import type {DescriptorWrapperFactoryByDescriptorTypeCtx, DescriptorWrapperFactoryByDescriptorType} from '@rnoh/react-native-openharmony/ts';
-import { RNC } from '@rnoh/react-native-openharmony/generated'
+import { RNC } from '@rnoh/react-native-openharmony/generated/ts'
 
 export class ImageSequencePackage extends RNPackage {
   createDescriptorWrapperFactoryByDescriptorType(ctx:
